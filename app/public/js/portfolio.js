056d4ac0b7d1ebbaef5cfebe1d3ae599241fc6dd
DOUGLAS LIMA DIAS <douglas.dias@centralit.com.br>
2019-04-30 10:44:26 -0300

    update

--- app/public/js/portfolio.js
+++ app/public/js/portfolio.js
@@ -62,8 +62,8 @@ var app = angular.module('portfolio', [])
 //about controller
 .controller('aboutCtrl', function($scope) {
   $scope.aboutText = {
-    title: "Hello there!",
-    copyText: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    title: "Olá!",
+    copyText: "Aqui vai uma breve informação It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
     facebookUrl: "",
     githubUrl: "",
     codepenUrl: ""
